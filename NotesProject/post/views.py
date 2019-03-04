8a7476f2232b8db6ae81b6cc63a745cff05ee824
Diego Salgado <dasalgadoc@gmail.com>
2019-03-03 22:42:26 -0500

    Avance

--- NotesProject/post/views.py
+++ NotesProject/post/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+
+from django.shortcuts import render
+
+posts = [
+    {
+        'name': 'Shiba',
+        'user': 'Diego Salgado',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://i.imgur.com/JXetxQh.jpg'
+    } ,
+    {
+        'name': 'Fence',
+        'user': 'Daniela S.',
+        'timestamp':  datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://i.imgur.com/kUCMPWX.jpg'
+    },
+    {
+        'name': 'Kiss',
+        'user': 'Alejandra P.',
+        'timestamp':  datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://i.imgur.com/cuS5DDv.jpg'
+    }
+]
 
 # Create your views here.
 
 def list_post(request):
-    return HttpResponse("I list!")
+    return render(request, 'feed.html', {'posts':posts})
