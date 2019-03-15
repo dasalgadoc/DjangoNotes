c5530dbefb9f4a34787550d84910fb8c84950a7b
Diego Salgado <dasalgadoc@gmail.com>
2019-03-14 22:41:15 -0500

    Nuevo commit

--- NotesProject/post/views.py
+++ NotesProject/post/views.py
@@ -12,16 +12,16 @@ posts = [
         'picture': 'https://i.pinimg.com/originals/01/c9/2e/01c92ec631bc743f88adb3520792cf8b.jpg'
     } ,
     {
-        'name': 'Fence',
+        'name': 'Beach time',
         'user': 'Daniela S.',
         'timestamp':  datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://i.imgur.com/kUCMPWX.jpg'
+        'picture': 'https://i.pinimg.com/564x/18/44/8e/18448e3b3365770bb9fa7d5ff97163f2.jpg'
     },
     {
-        'name': 'Kiss',
+        'name': 'My car',
         'user': 'Alejandra P.',
         'timestamp':  datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://i.imgur.com/cuS5DDv.jpg'
+        'picture': 'https://i.pinimg.com/564x/a7/8c/ae/a78cae4ad582b5afb7e71d1ef80a745f.jpg'
     }
 ]
 
