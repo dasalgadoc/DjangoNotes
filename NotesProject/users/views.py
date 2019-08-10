ae0ecb87aa55e41ba83e3f3ac536b045f2195f1e
Diego Salgado <dasalgadoc@gmail.com>
2019-08-10 16:07:23 -0500

    Manejo Formularios

--- NotesProject/users/views.py
+++ NotesProject/users/views.py
@@ -1,10 +1,17 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.shortcuts import redirect
-from django.contrib.auth.decorators import login_required
+# Django
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
+# Exception
 from django.db.utils import IntegrityError
+
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+# Forms
+from users.forms import ProfileForm
 
 # Create your views here.
 
@@ -57,4 +64,31 @@ def signup(request):
 
 @login_required
 def update_profile(request):
-    return render(request, 'users/update_profile.html')
+    """Update a user's profile view."""
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect('update_profile')
+
+    else:
+        form = ProfileForm()
+
+    return render(
+        request=request,
+        template_name='users/update_profile.html',
+        context={
+            'profile': profile,
+            'user': request.user,
+            'form': form
+        }
+    )
